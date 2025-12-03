fae78e22c775167c066c6fdc6aaed8b7513d6f6c
Selimcan Gürsu <selimcangursu@wikywatch.com.tr>
2025-12-03 19:52:47 +0300

    react-router-dom , tailwindcss ve react-icon kurkütüphaneleri eklendi. Header componenti tasarlandı ve responsive hale getirildi.

---
+++ website/src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './pages/Home'
+import Header from "./components/Header";
+function App() {
+  return (
+    <>
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ website/src/components/Header.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+
+const Header = () => {
+  return (
+    <div className="w-full flex flex-row shadow-2xl p-3 justify-between items-center">
+      <img src="" alt="logo" />
+      <nav className="hidden md:block">
+        <ul className="flex flex-row justify-center items-center gap-3">
+          <li>
+            <a href="">XYZ Estetik</a>
+          </li>
+          <li>
+            <a href="">Hizmetlerimiz</a>
+          </li>
+          <li>
+            <a href="">Hakkımızda</a>
+          </li>
+          <li>
+            <a href="">Blog</a>
+          </li>
+          <li>
+            <a href="">İletişim</a>
+          </li>
+        </ul>
+      </nav>
+      <ul>
+        <li className="hidden md:block">
+          <a className="bg-blue-950 text-white p-2 rounded-md text-sm" href="">
+            Hemen Ön Bilgi Al
+          </a>
+        </li>
+        <li className="block md:hidden">
+          <a href="">
+            <RxHamburgerMenu />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
